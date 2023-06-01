bfb86e5e0f42df34c24882c76d4fe8ef8921b784
Dillon Ellis <dillonzellis@gmail.com>
2023-06-01 15:37:42 -0400

    join cta styling

--- src/components/home/JoinCTA.tsx
+++ src/components/home/JoinCTA.tsx
@@ -6,9 +6,11 @@ const JoinCTA = () => {
     <ContentSection>
       <Container>
         <div className="flex justify-center">
-          <div className="flex max-w-prose flex-col gap-4 text-center">
-            <div className="text-3xl">Learn how to join MSCU today!</div>
-            <div>
+          <div className="flex max-w-prose flex-col gap-6 text-center">
+            <div className="text-4xl text-brandingBlue-400">
+              Learn how to join MSCU today!
+            </div>
+            <div className="text-lg">
               Join MSCU and use all of the free services that MSCU offers its
               members such as checking, home banking and bill pay services
             </div>
