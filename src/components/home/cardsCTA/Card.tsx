9e3428e9034cc8d482c032ee87476196d183ecc8
Dillon Ellis <dillonzellis@gmail.com>
2023-06-01 15:31:57 -0400

    cards sections

--- src/components/home/cardsCTA/Card.tsx
+++ src/components/home/cardsCTA/Card.tsx
@@ -1,13 +1,33 @@
-const Card = () => {
+interface CardProps {
+  icon: string;
+  title: string;
+  description?: string;
+  cardLink?: string;
+}
+
+const Card = ({
+  icon,
+  title,
+  description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, praesentium? Iure delectus officia reiciendis culpa facilis sequi explicabo expedita quia?",
+  cardLink = "#",
+}: CardProps) => {
   return (
-    <div className="inline-flex rounded-xl border-2 border-slate-600 bg-white p-4 text-black shadow-md">
-      <div className="flex flex-col gap-4">
-        <div className="text-black">Card Title</div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore sequi
-          laudantium praesentium nihil deserunt, ipsa eum deleniti.
-        </p>
-        <div>Learn More</div>
+    <div className="inline-flex h-[316px] rounded-3xl border border-slate-100 bg-white p-4 text-black shadow-md">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <img
+          width={70}
+          height={70}
+          src={icon}
+          alt=""
+          className="h-[70px] w-[70px] object-contain"
+        />
+        <div className="text-xl font-bold capitalize text-brandingBlue-400">
+          {title}
+        </div>
+        <p className="text-center">{description}</p>
+        <div className="font-bold text-brandingGreen-400">
+          <a href={cardLink}>Learn More</a>
+        </div>
       </div>
     </div>
   );
--- src/components/home/cardsCTA/CardDeck.tsx
+++ src/components/home/cardsCTA/CardDeck.tsx
@@ -2,11 +2,23 @@ import Card from "./Card";
 
 const CardDeck = () => {
   return (
-    <div className="absolute left-1/3 top-1/2 grid max-w-[800px] grid-cols-2 gap-6">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="absolute left-[40%] top-1/4 grid max-w-[800px] grid-cols-2 gap-6">
+      <Card
+        icon="http://dev2.growthbydesign.org/wp-content/uploads/loans-icon.png"
+        title="loan application"
+      />
+      <Card
+        icon="http://dev2.growthbydesign.org/wp-content/uploads/car-icon.png"
+        title="auto loans"
+      />
+      <Card
+        icon="http://dev2.growthbydesign.org/wp-content/uploads/atm-icon.png"
+        title="ATM/Shared Branching"
+      />
+      <Card
+        icon="http://dev2.growthbydesign.org/wp-content/uploads/fin-edu-icon.png"
+        title="financial education"
+      />
     </div>
   );
 };
